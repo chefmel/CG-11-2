9300788c6b509d56502bddcee2af815ce74ad427
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-09-28 12:28:15 -0500

    CG 11-2

---
+++ CG 11-2/GradeAverage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace StudentNames
+{
+    public class Student
+    {
+        public List<int> scores = new List<int>();
+        public string Name { get; set; }
+        public int Grades { get; set; }
+        public object Add { get; internal set; }
+
+        public Student(string name, int grades)
+        {
+            Name = name;
+            Grades = grades;
+        }
+        public void AddScore(int score)
+        {
+            scores.Add(score);
+        }
+        public double Average()
+        {
+            return scores.Sum() / scores.Count;
+        }
+
+    }
+}
